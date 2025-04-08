2a6646a8371dc78e1a308651e5fd79ca89480cb4
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-04-08 19:48:55 +0530

    SSl_Certificate Three

---
+++ src/test/java/HandlingSSL_Certificate/SSL_CertificateThree.java
@@ -0,0 +1,35 @@
+package HandlingSSL_Certificate;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SSL_CertificateThree {
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+
+		// Creating Chrome Options
+		ChromeOptions opt = new ChromeOptions();
+		opt.setAcceptInsecureCerts(true);
+		// Initiate ChromeDriver
+		driver = new ChromeDriver(opt);
+	}
+
+	@Test
+	public void sslCertificate() {
+		driver.get("https://untrusted-root.badssl.com/");
+		driver.manage().window().maximize();
+
+		System.out.println("Driver of the page: " + driver.getTitle());
+	}
+
+	@AfterClass()
+	public void tearDown() {
+		driver.quit();
+	}
+}
